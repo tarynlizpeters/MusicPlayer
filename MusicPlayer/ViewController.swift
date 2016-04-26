4219771509e99276a8885e512a717b80e1063bbc
Taryn Parker <tarynp@gmail.com>
2016-04-26 15:57:07 -0500

    first commit

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -7,12 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
+
+var audioPlayer = AVAudioPlayer()
+var isPlaying = false
+var timer:NSTimer!
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet var trackTime: UILabel!
+    @IBOutlet var trackTitle: UILabel!
+    
+
+
+    @IBAction func playPauseButton(sender: AnyObject) {
+        if isPlaying {
+            audioPlayer.pause()
+            isPlaying = false
+        } else {
+            audioPlayer.play()
+            isPlaying = true
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateTime", userInfo: nil, repeats: true)
+        }
+    }
+    
+    func updateTime() {
+        let currentTime = Int(audioPlayer.currentTime)
+        let minutes = currentTime/60
+        let seconds = currentTime - minutes * 60
+        
+        trackTime.text = NSString(format: "%02d:$02d", minutes, seconds) as String
+    }
+    
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0
+        isPlaying = false
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        trackTitle.text = "What's Up - 4 Non Blondes"
+        let path = NSBundle.mainBundle().URLForResource("What's Up - 4 Non Blondes", withExtension: "mp3")
+        
+        let audioPlayer = try? AVAudioPlayer(contentsOfURL: path!, fileTypeHint: [])
     }
 
     override func didReceiveMemoryWarning() {
